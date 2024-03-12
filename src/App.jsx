acb21a4138dfb8c1fe86e26da42cf4eb3db6f865
sajidhasan-web <sajidhasan.web@gmail.com>
2024-03-12 22:22:35 +0600

    initial file create

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-6xl bg-orange-400'>Knowledge Cafe</h1>
+      
+      
+    </>
+  )
+}
+
+export default App
