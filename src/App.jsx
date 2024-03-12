ef090441f89c23d01d3b09cc88e6be086f31bd77
sajidhasan-web <sajidhasan.web@gmail.com>
2024-03-12 23:34:42 +0600

    data loaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Header from './components/Header/Header'
 
 function App() {
   
 
   return (
     <>
-      <h1 className='text-6xl bg-orange-400'>Knowledge Cafe</h1>
-      
-      
+      <Header></Header>
+      <Blogs></Blogs>
     </>
   )
 }
---
+++ src/components/Blogs/Blogs.jsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+
+const Blogs = () => {
+
+    const [blogs, setBlogs] = useState([])
+
+    useEffect(()=>{
+        fetch('data.json')
+        .then(res => res.json())
+        .then(data => console.log(data))
+    },[])
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Blogs;
